c5d5bebc3c8e8b3f5d903633ae64574b2efa8803
curtis <curtisdb03@hotmail.com>
2016-09-29 13:33:22 -0600

    Add files via upload

---
+++ raycastingFloorCeiling-master/src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import teste3dfloor3.Game;
+
+/**
+ *
+ * @author leonardo
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.setProperty("sun.java2d.opengl", "true");
+        Game.show(800, 600);
+    }
+    
+}
